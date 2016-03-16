cd5d36430e1a4af55e98d5b6cdae2cfac4d4bede
cesleem <cesleem@gmail.com>
2016-03-15 23:37:07 -0700

    add get process logic

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var path       = require('path');
 mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGODB);
 
 var app = express();
-app.set('ip_address', (process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'));
+app.set('ip', (process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'));
 app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 5000));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -28,9 +28,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/process', function (req, res) {
+	res.json(process.env);
+});
+
 process.stdin.resume();
 process.stdin.on('data', homeController.sendPin);
 
-var server = app.listen(app.get('port'), app.get('ip_address'), function () {
+var server = app.listen(app.get('port'), app.get('ip'), function () {
   console.log('happiness-meter-server listening on port %s', app.get('port'));
 });
